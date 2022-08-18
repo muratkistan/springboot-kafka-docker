1e146e9eae6daeb371ac523129eacf543aef0f17
muratkistan <muratkistan@gmail.com>
2022-08-19 01:21:42 +0300

    Created User model class

---
+++ src/main/java/com/muratkistan/springbootkafkadocker/model/User.java
@@ -0,0 +1,14 @@
+package com.muratkistan.springbootkafkadocker.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+}
